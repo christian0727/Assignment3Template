04ddd7aaa340acb35d6902562924b7ec8f1ea056
Christian Yusi <ckdy_27@yahoo.com>
2016-02-29 15:16:19 -0600

    Create tables and inserts

--- A3WebApplication/A3WebApplication/Install.sql
+++ A3WebApplication/A3WebApplication/Install.sql
@@ -7,17 +7,98 @@
 -- stored procedures (20 marks total)
 -- reports (6 marks)
 
+USE MASTER
+GO
+DROP DATABASE dbAssignment3
+GO
+CREATE DATABASE dbAssignment3
+GO
+USE dbAssignment3
+GO
 CREATE TABLE tbCustomer -- AccessLevel is a BIT, 1 is admin, 0 is not an admin
+(
+	CustomerID		INT PRIMARY KEY IDENTITY (1,1),
+	FirstName		VARCHAR(MAX),
+	LastName		VARCHAR(MAX),
+	Address			VARCHAR(MAX),
+	City		    VARCHAR(MAX),
+	PhoneNumber		VARCHAR(MAX),
+	UserName		VARCHAR(MAX),
+	Password		VARCHAR(MAX),
+	AccessLevel		BIT
+)
 CREATE TABLE tbCategory
+(
+	CategoryID		INT PRIMARY KEY IDENTITY (1,1), 
+	Name			VARCHAR(MAX),
+	ImagePath		VARCHAR(MAX)
+)
 CREATE TABLE tbProduct -- there are many products in a single category
+(
+	ProductID		INT PRIMARY KEY IDENTITY (1,1),
+	Name			VARCHAR(MAX),
+	Prize			DECIMAL(7,2),
+	PrimaryImagePath VARCHAR(MAX),
+	CategoryID		INT REFERENCES tbCategory(CategoryID)
+)
 CREATE TABLE tbOrder -- an order happens on a date by a customer
+(
+	OrderID			INT PRIMARY KEY IDENTITY (1,1),
+	OrderDate		DATE,
+	PricePaid		DECIMAL(7,2),
+	CustomerID		INT REFERENCES tbCustomer(CustomerID)
+)
 CREATE TABLE tbOrderDetail -- there can be many details in an order, each detail contains the product purchased, the price paid at the time for the product multiplied by the quantity
+(
+	OrderDetailsID	INT PRIMARY KEY IDENTITY (1,1),
+	OrderID			INT REFERENCES tbOrder(OrderID),
+	ProductID		INT REFERENCES tbProduct(ProductID),
+	Quantity		INT,
+	Subtotal		DECIMAL(7,2)
+)
 
 INSERT INTO tbCustomer -- 5, 1 admin, 4 non-admins
+(FirstName, LastName, Address, City, PhoneNumber, UserName, Password, AccessLevel) VALUES
+('Christian', 'Yusi', 'Banning Street', 'Winnipeg', '204-234-2123', 'chyus', '1234', 1),
+('Frances', 'Yusi', 'White Street', 'Winnipeg', '204-123-4567', 'fravil', 'abcd', 0),
+('Earl', 'Lagaya', 'Blue Street', 'Winnipeg', '204-345-2314', 'earl', 'qwerty', 0),
+('Arjay', 'Candoy', 'Red Street', 'Winnipeg', '204-412-5673', 'arjay', 'zxcv', 0),
+('Michael', 'Olazo', 'Black Street', 'Winnipeg', '204-456-2222', 'michael', 'asdf', 0)
+
 INSERT INTO tbCategory -- 4 categories
+(Name, ImagePath) VALUES
+('Spirits', 'Spirits.jpg'),
+('Liqueurs', 'Liqueurs.jpg'),
+('Wines', 'Wines.jpg'),
+('Beers', 'Beer.jpg')
+
 INSERT INTO tbProduct  -- 6 products in category one, 3 products in category two, 1 in category three
+(Name, PrimaryImagePath, Prize, ProductID) VALUES
+('Mezcal', 'Mezcal.jpg', 58.12, 1),
+('St.George', 'St.George.jpg', 43.56, 1),
+('Teeling', 'Teeling.jpg', 44.25, 1),
+('Cognac', 'Cognac.jpg', 70.33, 1),
+('Pisco', 'Pisco.jpg', 30.13, 1),
+('MartinMiller', 'MartinMiller.jpg', 32.89, 1),
+('Molinari', 'Molinari.jpg', 43.23, 2),
+('Marlee', 'Marlee.jpg', 56.24, 2),
+('Somrus', 'Somrus.jpg', 34.78, 2),
+('PondView', 'PondView.jpg', 67.23, 3)
+
 INSERT INTO tbOrder    -- 3 example orders from the non-admins
+(CustomerID, OrderDate, PricePaid) VALUES
+(2, '02/25/2016', 88.50),
+(3, '02/27/2016', 157.92),
+(4, '02/29/2016', 164.59)
+
 INSERT INTO tbOrderDetail -- one item on the first order, 3 items on the second order, 2 items on the third order
+(OrderID, ProductID, Quantity, Subtotal) VALUES
+(1, 3, 2, 88.50),
+(2, 8, 1, 56.24),
+(2, 1, 1, 58.12),
+(2, 2, 1, 43.56),
+(3, 5, 1, 30.13),
+(3, 10, 2, 134.46)
 
 -- NOTE: All Insert procs *MUST* return the new identity number of the primary key.
 -- Example: if you use spInsertCategory after there are 4 categories, it should return the value 5.
